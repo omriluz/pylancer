0ba6126f1da1b38e7f5160360faa09015a3377eb
Omri Luz <omriluz1994@gmail.com>
2019-02-06 17:39:19 +0200

    removed commented block and introduced proper docstrings

--- pylancer.py
+++ pylancer.py
@@ -1,41 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
-''' 
-to make my code production ready by saturday i need to:
-handle scheduling for the scrapers either by sleep or a cron-job or any other method
-start working with a vcs!
-	go through files to see what you need and what you dont need 
-	create a private repo on github
-	upload project files
-document my code
-	learn how to document code
-	do it
-learn how to update and delete sql queries and do it
-rename files to make sense
-decide what to do with the footer for now
-
-work on at least one more scraper (this should be the last part you work on)
-'''
-
-# start working with vcs
-# if twago on job page refer to sign up with affiliate
-# pph banner in green bold with post a job css referring to pph affiliate
-# upload site to the web
-# use cloudflare on the site
-# use google analytics
-# UPDATE and DELETE sql queries accordingly
-# work on the rest of the scrapers
-# design main page...get rid of unnecessary spaces
-# email alerts for a certain search
-# smtp server with python for sending email alerts?
-# rename files to make sense
-# make a login/signup # database for login credentials needed
-# set cookies
-# make a pylancer gmail account
-# active search bar
-# try arrow module for time manipulation instead or in tandem with datetime module
-
 from flask import (Flask, render_template, url_for,
 				 redirect, request, make_response)
 from flask_obscure import Obscure
@@ -52,6 +16,9 @@ app.config['OBSCURE_SALT'] = 4049
 obscure = Obscure(app)
 
 def logofinder(site):
+
+	"returns the logo for the site given"
+	
 	if site == "People Per Hour":
 		return 'static/assets/img/peopleperhour-logo2.png'
 	elif site == 'Twago':
@@ -63,6 +30,9 @@ def logofinder(site):
 
 
 def currency_converter(price, currency):
+
+	"converts every currency given to its price in usd"
+
 	if price == 'price not mentioned':
 		return 'price not mentioned'
 	if ',' in price:
@@ -82,20 +52,10 @@ def currency_converter(price, currency):
 
 number_of_pages = sql.number_of_pages()
 
-
-# @app.route('/save')
-# def set_cookie():
-# 	resp = make_response(redirect(url_for('kaka')))
-# 	resp.set_cookie('cookie', 'shlomo')
-# 	return resp
-
-# @app.route('/kaka')
-# def kaka():
-# 	return request.cookies.get('cookie')
-
-
 @app.route("/")
 def index():
+	
+	"home page route"
 	
 	data = sql.get_page_posts()
 	nums = range(len(data))
@@ -108,6 +68,9 @@ def index():
 
 @app.route("/page=<int:page>")
 def pagination(page=1):
+
+	"handles pagination"
+	
 	if page == 1:
 		return redirect(url_for('index'))
 	data = sql.sort_by_time(page)
@@ -121,12 +84,19 @@ def pagination(page=1):
 
 @app.route('/<tame:job_id>', methods=['GET'])
 def job_page(job_id):
+	
+	"""a route that gets the job page 
+	with the details of each individual job"""
+
 	job_details = sql.job_details(job_id)
 	return render_template("job-page.html", job_details=job_details, logofinder=logofinder, currency_converter=currency_converter)
 
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
+
+	"search function for the search bar on the site"
+	
 	search_term = request.form['text'] 
 	data = sql.search_site(search_term)
 	nums = range(len(data))
